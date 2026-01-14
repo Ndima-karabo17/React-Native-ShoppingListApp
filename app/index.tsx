74fb78445db491cb6c31503e53eea85c605f2530
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2026-01-14 09:48:55 +0200

    Refactor imports and format JSX in Index component
    I fixed it to have a style the whole page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import {  StyleSheet, View } from "react-native";
-import ShoppingList from "./ShoppingList";
+import { View,StyleSheet } from 'react-native';
+import ShoppingList from './ShoppingList'; 
+import React from 'react';
 
 export default function Index() {
   return (
-   
-      <View style={styles.container}>
-        <ShoppingList />
-      </View>
-
+    <View style={styles.container}>
+      
+      <ShoppingList /> 
+    </View>
   );
 }
+
 const styles =StyleSheet.create({
   container: {
     flex: 1,
