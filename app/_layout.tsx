30eb1b77714564e7ebae2eab7e94f51f4b54ee2c
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2026-01-14 09:48:18 +0200

    Update _layout.tsx
    I added a component for store.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store"; // 1. Path to your store file (check if this is correct!)
+import React from "react";
 
 export default function RootLayout() {
-  return(
-    <Stack>
-       <Stack.Screen name="index" options={{headerShown:false}} />
-    </Stack>
+  return (
+
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </Provider>
   );
 }
